653cb09aff4bf18a11da10abc13cee82383df427
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-03-08 12:20:10 -0300

    Resources, models, controllers, factories e seeder para as tabelas do banco de dados

---
+++ app/Http/Controllers/ProductDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product_detail;
+use Illuminate\Http\Request;
+
+class ProductDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product_detail  $product_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product_detail $product_detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product_detail  $product_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product_detail $product_detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product_detail  $product_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product_detail $product_detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product_detail  $product_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product_detail $product_detail)
+    {
+        //
+    }
+}
