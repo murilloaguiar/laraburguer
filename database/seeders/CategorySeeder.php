a5348c4638c78bceac738895b1199113c7b7b64b
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-03-08 12:52:18 -0300

    Seeder inicial para as categorias. Rota resource para categoria

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,17 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+   /**
+    * Run the database seeds.
+    *
+    * @return void
+    */
+   public function run()
+   {
+      $categories = ['Refrigerantes','Lanches Artesanais','Cervejas','Porções','Agua','Sucos', 'Promoções', 'Combos'];
+
+      foreach($categories as $value) {
+         Category::create([
+            'name'=>$value
+         ]);
+      }
+      
+   }
 }
