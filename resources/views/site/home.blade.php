fc89488ff82f6a1bb06763ebbbe491592298628a
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-03-28 11:08:34 -0300

    Estilização inicial da home index

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -2,7 +2,33 @@
 
 @section('conteudo')
 
-   <h1>Teste</h1>
+   <section>
+      <header class="d-flex align-items-center" id="header-index">
+         <div class="row justify-content-center">
+            <div class="col-md-10">
+               <h1 class="display-4" id="texto-header-index">Algum texto legal bem aqui</h1>
+               <a class="btn  btn-lg btn-outline-warning" href="{{route('catalogo')}}">Cardápio</a>
+            </div>
+         </div>
+      </header>
+   </section>
+
+   <section class="container mt-4">
+      <div class="row">
+         <div class="col-4 d-flex flex-column align-items-center">
+            <i class="bi bi-heart display-3"></i>
+            <h5>Carinho</h5>
+         </div>
+         <div class="col-4 d-flex flex-column align-items-center">
+            <i class="bi bi-star display-3"></i>
+            <h5>Qualidade</h5>
+         </div>
+         <div class="col-4 d-flex flex-column align-items-center">
+            <i class="bi bi-hourglass display-3"></i>
+            <h5>Rapidez</h5>
+         </div>
+      </div>
+   </section>
 
 
 @endsection
--- resources/views/site/layouts/_partials/topo.blade.php
+++ resources/views/site/layouts/_partials/topo.blade.php
@@ -1,8 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <div class="container-fluid">
+<nav class="navbar navbar-expand-md fixed-top" id="menu">
+   <div class="container">
       <a class="navbar-brand" href="/"><img src="{{asset('img/Laravel.png')}}" width="70px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
+         <i class="bi bi-card-text display-6 text-white"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
          <ul class="navbar-nav">
