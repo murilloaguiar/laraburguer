aa3c110ae870a926961d5740517046c12ebc8072
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-03-28 12:18:49 -0300

    Adicionando seção e estilizando

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -13,21 +13,53 @@
       </header>
    </section>
 
-   <section class="container mt-4">
+   <section class="container mt-4" id="star-section">
       <div class="row">
-         <div class="col-4 d-flex flex-column align-items-center">
+         <div class="col-sm-12 col-md-4 d-flex flex-column">
             <i class="bi bi-heart display-3"></i>
             <h5>Carinho</h5>
+            <p class="text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, eveniet. </p>
          </div>
-         <div class="col-4 d-flex flex-column align-items-center">
+         <div class="col-sm-12 col-md-4 d-flex flex-column">
             <i class="bi bi-star display-3"></i>
             <h5>Qualidade</h5>
+            <p class="text-muted">Nihil expedita atque quo repellendus, distinctio quibusdam fugit ullam iusto possimus libero quae animi aut.</p>
          </div>
-         <div class="col-4 d-flex flex-column align-items-center">
+         <div class="col-sm-12 col-md-4 d-flex flex-column">
             <i class="bi bi-hourglass display-3"></i>
             <h5>Rapidez</h5>
+            <p class="text-muted">Nihil expedita atque quo repellendus, distinctio quibusdam fugit ullam iusto possimus libero quae animi aut.</p>
          </div>
       </div>
+   </section>
+
+   <section class="bg-dark" id="history-section">
+
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12 col-md-6">
+               <header>
+                  <h4>Sobre nós</h4>
+
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam eius ex ipsum illo sint numquam nostrum cupiditate nesciunt illum voluptas ea necessitatibus veniam doloribus fugit libero, laborum eaque atque quaerat?</p>
+                  <ul>
+                     <li>Algo</li>
+                     <li>Algo</li>
+                     <li>Algo</li>
+                     <li>Algo</li>
+                     <li>Algo</li>
+                  </ul>
+               </header>
+            </div>
+
+            <div class="col-sm-12 col-md-6 photo d-none d-md-block">
+               
+               <img src="{{asset('img/lanchonete-historia.jpg')}}" alt=""> 
+               
+            </div>
+         </div>
+      </div>
+      
    </section>
 
 
