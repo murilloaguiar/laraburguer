7f5cd45e1143959cbb86be2f80aa999fca0764a0
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-05-06 11:30:50 -0300

    Mostrando os produtos na página do carrinho e removendo o produto por lá

--- resources/views/site/carrinho.blade.php
+++ resources/views/site/carrinho.blade.php
@@ -1,31 +1,78 @@
 @extends('site.layouts.app')
 
+@section('css')
+   <link rel="stylesheet" href="{{asset('css/style-cart.css')}}">
+@endsection
+
 @section('conteudo')
 
-   <h1>Carrinho</h1>
+   @include('site.layouts._partials.topo')
+
+   <section>
+      <header class="d-flex align-items-center" id="header-cart">
+      </header>
+   </section>
+
+<ul class="list-group">
+   
+</ul>
 
 
 @endsection
 
 @section('js')
    <script>
-      let products = JSON.parse(localStorage.getItem('products'))
+      const products = JSON.parse(localStorage.getItem('products'))
+      console.log(products.length)
 
-      const myHeaders = {
-         headers:{
-            Accept: 'application/json'
+      if(products.length == 0){
+         const li = document.createElement('li')
+         li.className = "list-group-item"
+
+         li.innerHTML = "Sem itens no carrinho"
+         document.querySelector('.list-group').appendChild(li)
+
+      }else if(products.length > 0){
+
+         const myHeaders = {
+            headers:{
+               Accept: 'application/json'
+            }
          }
+
+         products.forEach(product => {
+            fetch(`http://localhost:8000/api/v1/product/${product.id}`)
+               .then(response=>response.json())
+               .then(data=>{
+                  const li = document.createElement('li')
+                  li.className = "list-group-item"
+                  li.innerHTML = data.name
+                  li.id = `product_${product.id}`
+
+                  const button = document.createElement('button')
+                  button.className = 'btn btn-danger'
+                  button.innerHTML = 'remover'
+                  button.id = product.id
+
+                  button.onclick = (event)=>{
+                     // console.log(event.target)
+                     products.map((product, index)=>{
+                        if(product.id==event.target.id){
+                           products.splice(index, 1)
+                        }
+                     })
+
+                     localStorage.setItem('products', JSON.stringify(products))
+                     document.getElementById(`product_${event.target.id}`).remove()
+                  }
+    
+                  li.appendChild(button)
+
+                  document.querySelector('.list-group').appendChild(li)
+               })
+
+         });
       }
-
-      products.forEach(product => {
-         fetch(`http://localhost:8000/api/v1/product/${product.id}`)
-            .then(response=>response.json())
-            .then(data=>console.log(data))
-
-
-
-
-      });
 
 
 
